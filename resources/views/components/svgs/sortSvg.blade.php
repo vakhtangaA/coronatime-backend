821ed694a860f4b70740b4a9da93dc48cdb99bac
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-12-21 19:20:35 +0400

    chore: change sort icon source to svg

--- resources/views/components/svgs/sortSvg.blade.php
+++ resources/views/components/svgs/sortSvg.blade.php
@@ -1,17 +1,23 @@
-<svg xmlns="http://www.w3.org/2000/svg"
-     class="icon icon-tabler icon-tabler-arrows-sort inline-block"
-     width="20"
-     height="20"
-     viewBox="0 0 24 24"
-     stroke-width="1.5"
-     stroke="#2c3e50"
-     fill="none"
-     stroke-linecap="round"
-     stroke-linejoin="round">
-  <path stroke="none"
-        d="M0 0h24v24H0z"
-        fill="none" />
-  <path d="M3 9l4 -4l4 4m-4 -4v14"
-        stroke="#431413" />
-  <path d="M21 15l-4 4l-4 -4m4 4v-14" />
-</svg>
+<div class="inline-block ml-2">
+  <a href="{{ request()->fullUrlWithQuery(['sort' => $term . '-asc']) }}">
+    <svg width="10"
+         height="6"
+         viewBox="0 0 10 6"
+         fill="none"
+         class="mb-1"
+         xmlns="http://www.w3.org/2000/svg">
+      <path d="M5 0.5L10 5.5L0 5.5L5 0.5Z"
+            fill="{{ request()->get('sort') === $term . '-asc' ? 'black' : '#BFC0C4' }}" />
+    </svg>
+  </a>
+  <a href="{{ request()->fullUrlWithQuery(['sort' => $term . '-desc']) }}">
+    <svg width="10"
+         height="6"
+         viewBox="0 0 10 6"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+      <path d="M5 5.5L0 0.5H10L5 5.5Z"
+            fill="{{ request()->get('sort') === $term . '-desc' ? 'black' : '#BFC0C4' }}" />
+    </svg>
+  </a>
+</div>
