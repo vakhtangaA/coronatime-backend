47824ea991b5e307ae24ab3bbdb6a6c0d9343636
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-12-21 19:25:41 +0400

    feat: implement search and sort functionality

--- app/Http/Livewire/Components/ByCountry.php
+++ app/Http/Livewire/Components/ByCountry.php
@@ -7,14 +7,77 @@ use Livewire\Component;
 
 class ByCountry extends Component
 {
-	public $countries;
+	public $search;
 
 	public function render()
 	{
-		$this->countries = Country::all();
+		$countries = Country::all();
+
+		$countries = Country::where('name', 'like', '%' . $this->search . '%')
+		->get()
+		->when(
+			request()->get('sort') === 'location-desc',
+			function () use ($countries) {
+				return $countries->sortByDesc('name');
+			}
+		)
+		->when(
+			request()->get('sort') === 'location-asc',
+			function () use ($countries) {
+				return $countries->sortBy('name');
+			}
+		)
+		->when(
+			request()->get('sort') === 'cases-desc',
+			function () use ($countries) {
+				return $countries->sortByDesc('confirmed');
+			}
+		)
+		->when(
+			request()->get('sort') === 'cases-asc',
+			function () use ($countries) {
+				return $countries->sortBy('confirmed');
+			}
+		)
+		->when(
+			request()->get('sort') === 'deaths-desc',
+			function () use ($countries) {
+				return $countries->sortByDesc('deaths');
+			}
+		)
+		->when(
+			request()->get('sort') === 'deaths-asc',
+			function () use ($countries) {
+				return $countries->sortBy('deaths');
+			}
+		)
+		->when(
+			request()->get('sort') === 'recovered-desc',
+			function () use ($countries) {
+				return $countries->sortByDesc('recovered');
+			}
+		)
+		->when(
+			request()->get('sort') === 'recovered-asc',
+			function () use ($countries) {
+				return $countries->sortBy('recovered');
+			}
+		)
+		->when(
+			request()->get('sort') === 'critical-desc',
+			function () use ($countries) {
+				return $countries->sortByDesc('critical');
+			}
+		)
+		->when(
+			request()->get('sort') === 'critical-asc',
+			function () use ($countries) {
+				return $countries->sortBy('critical');
+			}
+		);
 
 		return view('livewire.components.by-country', [
-			'countries' => $this->countries,
-		]);
+			'countries' => $countries,
+		])->layout('layout');
 	}
 }
