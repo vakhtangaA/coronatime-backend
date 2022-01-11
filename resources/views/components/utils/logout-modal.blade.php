cac7b681ddc90a2b1968192cb0d3b478b61b3f0a
vakhtangaa <vakhtang.chitauri@gmail.com>
2022-01-11 18:29:07 +0400

    style: separate logout word

--- resources/views/components/utils/logout-modal.blade.php
+++ resources/views/components/utils/logout-modal.blade.php
@@ -21,11 +21,11 @@
         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
           <h3 class="text-lg font-medium leading-6 text-gray-900"
               id="modal-title">
-            {{__("Logout")}}
+            {{ __('Log out') }}
           </h3>
           <div class="mt-2">
             <p class="text-sm text-gray-500">
-              {{__("Are you sure you want to logout?")}}
+              {{ __('Are you sure you want to logout?') }}
             </p>
           </div>
         </div>
@@ -36,14 +36,14 @@
           @csrf
           <button type="submit"
                   class="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-            {{__("Logout")}}
+            {{ __('Log out') }}
           </button>
         </form>
 
         <button type="button"
                 @click="show = false"
                 class="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">
-          {{__("Cancel")}}
+          {{ __('Cancel') }}
         </button>
       </div>
     </div>
