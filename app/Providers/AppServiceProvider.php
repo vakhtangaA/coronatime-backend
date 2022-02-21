2b007f8a0b0ab1e560f5b99b60e4d4a1412aec60
vakhtangaa <vakhtang.chitauri@gmail.com>
2022-02-21 21:24:40 +0400

    refactor: make telescope run only on local env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,25 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		if (class_exists(TelescopeApplicationServiceProvider::class))
+		{
+			$this->app->register(TelescopeServiceProvider::class);
+		}
+	}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+	}
 }
